a9fb1a8cc67c6a1a28401a82ecac809016a3ad87
masterexploder <ian@gxdlabs.com>
2012-09-11 10:02:58 -0700

    Adding nodejs cookbook

---
+++ cookbooks/nodejs/README.md
@@ -0,0 +1,21 @@
+Description
+===========
+
+Installs nodejs on the system.
+
+Requirements
+============
+
+None
+
+Attributes
+==========
+
+The only attribute we track for node is the version:
+
+* `node[:nodejs][:version]` - Set this to the desired version to download
+
+Usage
+=====
+
+Simply include the recipe, and it'll get installed.
---
+++ cookbooks/nodejs/recipes/default.rb
@@ -0,0 +1,42 @@
+#
+# Cookbook Name:: nodejs
+# Recipe:: default
+#
+# Copyright 2012, Ian Selby
+#
+
+package "libssl-dev"
+package "libexpat1-dev"
+
+node_version           = node[:nodejs][:version]
+node_version_installed = `node -v | awk '{print $1}'`.strip.gsub('v', '')
+
+node_already_installed = lambda do
+  Chef::Log.debug("node version installed: #{node_version_installed}")
+  Chef::Log.debug("node version: #{node_version}")
+  node_version_installed == node_version
+end
+
+remote_file "/src/node-v#{node_version}.tar.gz" do
+  source "http://nodejs.org/dist/v#{node_version}/node-v#{node_version}.tar.gz"
+  not_if { File.directory?("/src/node-v#{node_version}") }
+end
+
+execute "tar -zxvf /src/node-v#{node_version}.tar.gz" do
+  cwd "/src"
+  not_if &node_already_installed
+end
+
+execute "cd /src/node-v#{node_version} && ./configure" do
+  not_if &node_already_installed
+end
+
+execute "make" do
+  cwd "/src/node-v#{node_version}"
+  not_if &node_already_installed
+end
+
+execute "make install" do
+  cwd "/src/node-v#{node_version}"
+  not_if &node_already_installed
+end
